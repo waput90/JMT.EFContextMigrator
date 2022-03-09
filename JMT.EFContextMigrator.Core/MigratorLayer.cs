dfacf09a36825d75abb0dd0f949627cd5f68717b
James <james.tubiano@interactivepartners.co.au>
2022-03-09 22:26:49 +0800

    initial commit

---
+++ JMT.EFContextMigrator.Core/MigratorLayer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace JMT.EFContextMigrator.Core
+{
+    public static class MigratorLayer
+    {
+        public static IApplicationBuilder RegisterMigratorMiddleware<TContext>(this IApplicationBuilder app) where TContext: DbContext
+        {
+            try
+            {
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<TContext>();
+                    db.Database.Migrate();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+            return app;
+        }
+    }
+}
